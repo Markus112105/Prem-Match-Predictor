e3decc118ea3f49597595a407d176820f12a24fc
markus112105 <markusmassina@gmail.com>
2025-01-26 18:14:26 -0500

    line9

--- Scraping.py
+++ Scraping.py
@@ -6,7 +6,7 @@ import time
 standings_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
 data = requests.get(standings_url)
 
-soup = BeautifulSoup(data.text)
+soup = BeautifulSoup(data.text, features="lxml")
 standings_table = soup.select('table.stats_table')[0]
 links = standings_table.find_all('a')
 links = [l.get("href") for l in links]
